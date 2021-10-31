8e584816f701e8727b63da6ff581362dfdbe1798
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2021-10-31 18:19:15 +0000

    haiku: add media-kit backend

---
+++ mediakit-sys/build.rs
@@ -0,0 +1,51 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+        // Tell cargo to tell rustc to link the system libmedia
+        // shared libary.
+        println!("cargo:rustc-link-lib=dylib=media");
+
+        // Tell cargo to invalidate the built crate whenever the
+        // wrapper changes.
+        println!("cargo:rerun-if-changed=wrapper.h");
+
+        // The bindgen::Builder is the main entry point to
+        // bindgen, and lets you build up options for the
+        // resulting bindings.
+        let bindings = bindgen::Builder::default()
+                // The input header we would like to generate
+                // bindings for.
+                .header("wrapper.h")
+                // Tell cargo to invalidate the built create
+                // whenever any of the included header files
+                // changed.
+                .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+                // Adapted from asio binding build script...
+                .clang_arg("-x")
+                .clang_arg("c++")
+                // C++ stdlib headers missing, so add
+                .clang_arg("-I")
+                .clang_arg("/system/develop/tools/lib/gcc/x86_64-unknown-haiku/8.3.0/include/c++")
+                .clang_arg("-I")
+                .clang_arg("/system/develop/tools/lib/gcc/x86_64-unknown-haiku/8.3.0/include/c++/x86_64-unknown-haiku")
+                // Allow specific types, etc.
+                .allowlist_type("BMediaRoster")
+                .allowlist_type("BBuffer")
+                .allowlist_type("BMediaNode")
+                .allowlist_type("media_node")
+                .allowlist_type("media_format")
+                .allowlist_type("media_output")
+                // Finish the builder and generate the bindings.
+                .generate()
+                // Unwrap the Result and panic on failure.
+                .expect("Unable to generate bindings");
+
+        // Write the bindings to the $OUT_DIR/bindings.rs file.
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+                .write_to_file(out_path.join("bindings.rs"))
+                .expect("Couldn't write bindings!");
+}
---
+++ mediakit-sys/mediakit-link/wrapper.h
@@ -0,0 +1,5 @@
+#include <MediaRoster.h>
+#include <MediaFormats.h>
+#include <MediaNode.h>
+#include <MediaEventLooper.h>
+#include <Buffer.h>
